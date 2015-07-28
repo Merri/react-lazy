8017f2ec6bc571895047a50915f834e2bbc34d48
Vesa Piittinen <vesa.piittinen@gmail.com>
2015-07-28 19:27:19 +0300

    Make it actually work

---
+++ demo/app.js
@@ -0,0 +1,63 @@
+(function() {
+    var Lazy = React.createFactory(window.Lazy)
+
+    var TRANSPARENT_GIF = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7'
+
+    var ImageLink = React.createFactory(React.createClass({
+        displayName: 'ImageLink',
+
+        onError: function() {
+            console.log('Load ' + this.props.src + ' FAILED', new Date())
+        },
+
+        onLoad: function() {
+            console.log('Load ' + this.props.src, new Date())
+        },
+
+        onLoaded: function() {
+            console.log('Load ' + this.props.src + ' complete', new Date())
+        },
+
+        render: function() {
+            return Lazy(
+                {
+                    nodeName: 'a',
+                    href: this.props.href || '#',
+                    className: 'image-link image-link--100px',
+                    onLoad: this.onLoad
+                },
+                React.DOM.img({
+                    className: 'image-link__image',
+                    alt: this.props.alt || '',
+                    height: this.props.height,
+                    src: this.props.src || TRANSPARENT_GIF,
+                    width: this.props.width,
+                    onLoad: this.onLoaded,
+                    onError: this.onError
+                })
+            )
+        }
+    }))
+
+    var Demo = React.createClass({
+        displayName: 'Demo',
+
+        render: function() {
+            return React.DOM.div(
+                {},
+                ImageLink({ src: 'http://placekitten.com/50/75' }),
+                ImageLink({ src: 'http://placekitten.com/60/75' }),
+                ImageLink({ src: 'http://placekitten.com/70/75' }),
+                ImageLink({ src: 'http://placekitten.com/80/75' }),
+                ImageLink({ src: 'http://placekitten.com/90/75' }),
+                ImageLink({ src: 'http://placekitten.com/100/75' }),
+                ImageLink({ src: 'http://placekitten.com/75/75' }),
+                ImageLink({ src: 'http://placekitten.com/75/85' }),
+                ImageLink({ src: 'http://placekitten.com/75/95' }),
+                ImageLink({ src: 'http://placekitten.com/75/55' })
+            )
+        }
+    })
+
+    React.render(React.createElement(Demo), document.getElementById('app'))
+})()
--- index.js
+++ index.js
@@ -1,206 +1,226 @@
-'use strict'
-var React = require('react')
-var verge = require('verge')
+(function(make) {
+    make = make()
 
-var elements = [],
-    isBind = false
-
-function throttle(func, wait) {
-    var timeout
-
-    return function() {
-        if (!timeout) {
-            timeout = setTimeout(function() {
-                timeout = null
-            }, wait)
-
-            func.apply(this, arguments)
-        }
-    }
-}
-
-var checkElementsInViewport = throttle(function() {
-    for (var i = elements.length; i > 0; --i) {
-        if (verge.inViewport(elements[i].element, elements[i].cushion)) {
-            elements[i].callback()
-            elements.splice(i, 1)
-            checkUnbind()
-        }
-    }
-}, 50)
-
-function checkUnbind() {
-    if (isBind && elements.length === 0 && window.removeEventListener) {
-        window.removeEventListener('resize', checkElementsInViewport, false)
-        window.removeEventListener('scroll', checkElementsInViewport, false)
-        window.removeEventListener('touchend', checkElementsInViewport, false)
-        isBind = false
-    }
-}
-
-function addElement(options) {
-    if (verge.inViewport(options.element, options.cushion)) {
-        options.callback()
-        return
+    if (typeof module != 'undefined' && module['exports']) {
+        module['exports'] = make
     }
 
-    if (!isBind && elements.length === 0 && window.addEventListener) {
-        window.addEventListener('resize', checkElementsInViewport, false)
-        window.addEventListener('scroll', checkElementsInViewport, false)
-        window.addEventListener('touchend', checkElementsInViewport, false)
-        isBind = true
-    }
-
-    elements.push(options)
-}
-
-function removeElement(options) {
-    var index = elements.indexOf(options)
-
-    if (index >= 0) {
-        elements.splice(index, 1)
-    }
-
-    checkUnbind()
-}
-
-var Lazy = React.createClass({
-    displayName: 'Lazy',
-
-    propTypes: {
-        children: React.PropTypes.oneOfType([
-            React.PropTypes.arrayOf(React.PropTypes.node),
-            React.PropTypes.node
-        ]),
-        cushion: React.PropTypes.number,
-        nodeName: React.PropTypes.oneOf([
-            'a',
-            'abbr',
-            'address',
-            'article',
-            'aside',
-            'b',
-            'bdi',
-            'bdo',
-            'big',
-            'blockquote',
-            'body',
-            'button',
-            'caption',
-            'cite',
-            'code',
-            'data',
-            'dd',
-            'del',
-            'dfn',
-            'dialog',
-            'div',
-            'dt',
-            'em',
-            'fieldset',
-            'figcaption',
-            'footer',
-            'form',
-            'h1',
-            'h2',
-            'h3',
-            'h4',
-            'h5',
-            'h6',
-            'header',
-            'i',
-            'ins',
-            'kbd',
-            'label',
-            'legend',
-            'li',
-            'main',
-            'mark',
-            'menuitem',
-            'nav',
-            'ol',
-            'output',
-            'p',
-            'pre',
-            'q',
-            's',
-            'samp',
-            'section',
-            'small',
-            'span',
-            'strong',
-            'summary',
-            'td',
-            'th',
-            'time',
-            'u',
-            'ul',
-            'var'
-        ]),
-        onLoad: React.PropTypes.func
-    },
-
-    getDefaultProps: function() {
-        return {
-            cushion: 0,
-            nodeName: 'div'
-        }
-    },
-
-    getInitialState: function() {
-        return {}
-    },
-
-    componentDidMount: function() {
-        this.options = {
-            callback: this.handleLoad,
-            cushion: this.props.cushion,
-            element: React.findDOMNode(this)
-        }
-        addElement(this.options)
-    },
-
-    componentWillUnmount: function() {
-        removeElement(this.options)
-    },
-
-    handleLoad: function() {
-        if (this.props.onLoad) {
-            this.props.onLoad()
-        }
-        this.setState({ loadedAt: Date.now() })
-    },
-
-    render: function() {
-        var props = {}
-
-        for (var key in this.props) {
-            if (this.props.hasOwnProperty(key) && !Lazy.propTypes.hasOwnProperty(key)) {
-                props[key] = this.props[key]
+    if (typeof window !== 'undefined' && window.React) {
+        for (var key in make) {
+            if (make.hasOwnProperty(key)) {
+                if (!window[key]) {
+                    window[key] = make[key]
+                }
             }
         }
-
-        if (this.state.loadedAt) {
-            return React.createElement(this.props.nodeName, props, this.props.children)
-        }
-
-        props.dangerouslySetInnerHTML = {
-            __html: React.renderToStaticMarkup(
-                React.DOM.noscript({}, this.props.children)
-            ).replace(
-                '<noscript>',
-                '<!--[if IE 9]><!--><noscript><!--<![endif]-->'
-            ).replace(
-                '</noscript>',
-                '<!--[if IE 9]><!--></noscript><!--<![endif]-->'
-            )
-        }
-
-        return React.createElement(this.props.nodeName, props)
     }
-})
+})(function() {
+    'use strict'
+    var React = require('react')
+    var verge = require('verge')
 
-module.exports = {
-    Lazy: Lazy,
-    verge: verge
-}
+    var PLAUSIBLE_NOSCRIPT_CONTAINERS = [
+        'a',
+        'abbr',
+        'address',
+        'article',
+        'aside',
+        'b',
+        'bdi',
+        'bdo',
+        'big',
+        'blockquote',
+        'body',
+        'button',
+        'caption',
+        'cite',
+        'code',
+        'data',
+        'dd',
+        'del',
+        'dfn',
+        'dialog',
+        'div',
+        'dt',
+        'em',
+        'fieldset',
+        'figcaption',
+        'footer',
+        'form',
+        'h1',
+        'h2',
+        'h3',
+        'h4',
+        'h5',
+        'h6',
+        'header',
+        'i',
+        'ins',
+        'kbd',
+        'label',
+        'legend',
+        'li',
+        'main',
+        'mark',
+        'menuitem',
+        'nav',
+        'ol',
+        'output',
+        'p',
+        'pre',
+        'q',
+        's',
+        'samp',
+        'section',
+        'small',
+        'span',
+        'strong',
+        'summary',
+        'td',
+        'th',
+        'time',
+        'u',
+        'ul',
+        'var'
+    ]
+
+    var elements = [],
+        isBind = false
+
+    function throttle(func, wait) {
+        var timeout
+
+        return function() {
+            if (!timeout) {
+                timeout = setTimeout(function() {
+                    timeout = null
+                }, wait)
+
+                func.apply(this, arguments)
+            }
+        }
+    }
+
+    var checkElementsInViewport = throttle(function() {
+        for (var i = elements.length - 1; i >= 0; i--) {
+            if (verge.inViewport(elements[i].element, elements[i].cushion)) {
+                elements[i].callback()
+                elements.splice(i, 1)
+                checkUnbind()
+            }
+        }
+    }, 50)
+
+    function checkUnbind() {
+        if (isBind && elements.length === 0 && window.removeEventListener) {
+            window.removeEventListener('resize', checkElementsInViewport, false)
+            window.removeEventListener('scroll', checkElementsInViewport, false)
+            window.removeEventListener('touchend', checkElementsInViewport, false)
+            isBind = false
+        }
+    }
+
+    function addElement(options) {
+        if (verge.inViewport(options.element, options.cushion)) {
+            options.callback()
+            return
+        }
+
+        if (!isBind && elements.length === 0 && window.addEventListener) {
+            window.addEventListener('resize', checkElementsInViewport, false)
+            window.addEventListener('scroll', checkElementsInViewport, false)
+            window.addEventListener('touchend', checkElementsInViewport, false)
+            isBind = true
+        }
+
+        elements.push(options)
+    }
+
+    function removeElement(options) {
+        var index = elements.indexOf(options)
+
+        if (index >= 0) {
+            elements.splice(index, 1)
+        }
+
+        checkUnbind()
+    }
+
+    var Lazy = React.createClass({
+        displayName: 'Lazy',
+
+        propTypes: {
+            children: React.PropTypes.oneOfType([
+                React.PropTypes.arrayOf(React.PropTypes.node),
+                React.PropTypes.node
+            ]),
+            cushion: React.PropTypes.number,
+            nodeName: React.PropTypes.oneOf(PLAUSIBLE_NOSCRIPT_CONTAINERS),
+            onLoad: React.PropTypes.func
+        },
+
+        getDefaultProps: function() {
+            return {
+                cushion: 0,
+                nodeName: 'div'
+            }
+        },
+
+        getInitialState: function() {
+            return {}
+        },
+
+        componentDidMount: function() {
+            this.options = {
+                callback: this.handleLoad,
+                cushion: this.props.cushion,
+                element: React.findDOMNode(this)
+            }
+            addElement(this.options)
+        },
+
+        componentWillUnmount: function() {
+            removeElement(this.options)
+        },
+
+        handleLoad: function() {
+            if (this.props.onLoad) {
+                this.props.onLoad()
+            }
+            this.setState({ loadedAt: Date.now() })
+        },
+
+        render: function() {
+            var props = {}
+
+            for (var key in this.props) {
+                if (this.props.hasOwnProperty(key) && !Lazy.propTypes.hasOwnProperty(key)) {
+                    props[key] = this.props[key]
+                }
+            }
+
+            if (this.state.loadedAt) {
+                return React.createElement(this.props.nodeName, props, this.props.children)
+            }
+
+            props.dangerouslySetInnerHTML = {
+                __html: React.renderToStaticMarkup(
+                    React.DOM.noscript({}, this.props.children)
+                ).replace(
+                    '<noscript>',
+                    '<!--[if IE 9]><!--><noscript><!--<![endif]-->'
+                ).replace(
+                    '</noscript>',
+                    '<!--[if IE 9]><!--></noscript><!--<![endif]-->'
+                )
+            }
+
+            return React.createElement(this.props.nodeName, props)
+        }
+    })
+
+    return {
+        Lazy: Lazy,
+        verge: verge
+    }
+});

4d28ffee10902ce49b79f48a4f80ef1da7d2ca1f
Vesa Piittinen <vesap@verkkokauppa.com>
2017-07-24 11:59:57 +0300

    Adjust linting rules: allow desctructured unused vars

--- .eslintrc.js
+++ .eslintrc.js
@@ -50,6 +50,14 @@ module.exports = {
                 "allowEmptyCatch": true
             }
         ],
+        "no-unused-vars": [
+            "error",
+            {
+                "vars": "all",
+                "args": "after-used",
+                "ignoreRestSiblings": true
+            }
+        ],
         "quotes": [
             "error",
             "single"
